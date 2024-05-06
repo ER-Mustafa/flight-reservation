4d56a91484e75080d27d4dd9daf45de1b7fa369b
ER-Mustafa <mer20@ku.edu.tr>
2024-05-06 10:39:29 +0300

    Added passenger adding functionality.

--- src/handlers/TerminalHandler.java
+++ src/handlers/TerminalHandler.java
@@ -1,10 +1,25 @@
 package handlers;
 
 import system.Flight;
+import system.Passenger;
 
 import java.util.Scanner;
 
 public class TerminalHandler implements Runnable {
+    /*
+    * HANDBOOK
+    * COMMANDS:
+    *
+    * exit
+    * query date: queries flights on this date --- date format DD/MM/YYYY
+    * create-user
+    * book flightNumber for passengerId
+    * capacity flightNumber
+    * show-reservations passengerId
+    * show-ticket passengerId
+    *
+    * */
+
     @Override
     public void run() {
         System.out.println("WELCOME TO RS13+1! Please provide a command: ");
@@ -20,6 +35,23 @@ public class TerminalHandler implements Runnable {
             if (elements[0].equals("query")) {
                 String date = elements[1];
                 Flight.printFlightsByDate(date);
+            }
+
+            if (elements[0].equals("create-user")) {
+                System.out.println("Enter name,age,surname,nationality, gender, nationalId");
+                System.out.print("-> ");
+                String information = scanner.nextLine();
+                String[] infoArray = information.split(",");
+                String name = infoArray[0];
+
+                int age = Integer.parseInt(infoArray[1]); // Convert this into integer
+                String surname = infoArray[2];
+                String nationality = infoArray[3];
+                String gender = infoArray[4];
+                String nationalId = infoArray[5];
+
+                Passenger newPassenger = new Passenger(name, age, surname, nationality, gender, nationalId);
+                System.out.println("Passenger created successfully!");
             }
         }
     }
--- src/system/Passenger.java
+++ src/system/Passenger.java
@@ -1,17 +1,23 @@
 package system;
 
+import java.util.ArrayList;
+
 public class Passenger extends Client {
     private static int idCounter = 0;
     private final int id;
+    private static final ArrayList<Passenger> passengers = new ArrayList<>();
 
     public Passenger(String name, int age, String surname,
                      String nationality, String gender, String nationalId ) {
         super(name, age, surname, nationality, gender, nationalId);
         Passenger.idCounter++;
         this.id = idCounter;
+        Passenger.passengers.add(this);
     }
 
     public int getId() {
         return id;
     }
+
+    public static ArrayList<Passenger> getPassengers() { return passengers; }
 }
